8f617fcd96ba4f544087f54750f6f7798a6e072e
Thiago Maia da Costa <thiago.maia971@gmail.com>
2017-11-17 19:08:16 -0300

    Ajustes na renderização dos componentes

--- src/app/services/resultado-event.service.ts
+++ src/app/services/resultado-event.service.ts
@@ -8,14 +8,24 @@ export class ResultadoEventService {
   private resultadoEventSource = new ReplaySubject<DadosMelhorCromossomo>();
   public handleAdd: Observable<any> = this.resultadoEventSource.asObservable();
   public resultados: DadosMelhorCromossomo[] = [];
+
+  public get melhorResultado() {
+    return this.resultados.sort((a, b) => {
+        if (a.cromossomo.fitness > b.cromossomo.fitness)
+          return 1;
+        if (a.cromossomo.fitness < b.cromossomo.fitness)
+          return -1
+        return 0;
+      })[0];
+  }
   
-  
-  public add(cromossomo: Cromossomo, geracoes: number, tempo: string, time: number){
+  public add(cromossomo: Cromossomo, geracoes: number, tempo: string, time: number, indexSolucao: number){
     let dados: DadosMelhorCromossomo = {
       cromossomo,
       geracoes,
       tempo,
-      time
+      time,
+      indexSolucao
     };
     this.resultados.push(dados);
     this.resultadoEventSource.next(dados);
@@ -26,5 +36,6 @@ export class DadosMelhorCromossomo {
   public cromossomo: Cromossomo;
   public geracoes: number;
   public tempo: string;
-  public time: number
+  public time: number;
+  public indexSolucao: number;
 }
